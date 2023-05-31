9348ed22aa515355c361b4fb078d8d3b7dd1df21
Ait-Mlouk Omar <72560701+omarait-mlouk@users.noreply.github.com>
2023-05-31 21:56:22 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Kickoff project',
+    'version': '1.0',
+    'summary': 'A starter project',
+    'description': """
+        A basic module to start your module.
+    """,
+    'author': 'Ait-Mlouk Omar',
+    'website': '',
+    'category': 'Category',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/menu.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'AGPL-3',
+}
